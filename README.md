57035d493b6d153efd70ff9eb082ee20548e29e4
Eric Lagergren <eric@ericlagergren.com>
2021-12-27 14:09:07 -0800

    fuzz test against the reference C implementation
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- README.md
+++ README.md
@@ -5,6 +5,87 @@
 
 <p align="center">NIST Lightweight Cryptography</p>
 
-```go
-import "github.com/ericlagergren/lwcrypto"
+This module implements NIST Lightweight Cryptography finalists.
+
+## Installation
+
+Each implementation can be installed using Go modules. For
+example:
+
+```bash
+go get github.com/ericlagergren/lwcrypto@latest
 ```
+
+## Usage
+
+The APIs conform to Go's `crypto/cipher` package. Note that the
+following example is not a substitute for reading the package's
+documentation.
+
+```go
+import (
+	"crypto/rand"
+
+	"github.com/ericlagergren/lwcrypto/ascon"
+)
+
+func main() {
+	// Keys must be KeySize bytes long. Anything else is an
+	// error.
+	key := make([]byte, ascon.KeySize)
+	if _, err := rand.Read(key); err != nil {
+		// rand.Read failing is almost always catastrophic.
+		panic(err)
+	}
+
+	// Nonces must be NonceSize bytes long. Anything else is an
+	// error.
+	nonce := make([]byte, ascon.NonceSize)
+	if _, err := rand.Read(nonce); err != nil {
+		// rand.Read failing is almost always catastrophic.
+		panic(err)
+	}
+
+	aead, err := ascon.New128(key)
+	if err != nil {
+		// New128 (and New128a) should only return an error if
+		// the key is not KeySize bytes long.
+		panic(err)
+	}
+
+	// Plaintext is encrypted and authenticated.
+	plaintext := []byte("example plaintext")
+
+	// Additional data is authenticated alongside the plaintext,
+	// but not included in the ciphertext.
+	additionalData := []byte("example additional authenticated data")
+
+	// Encrypt and authenticate |plaintext| and authenticate
+	// |additionalData|.
+	ciphertext := aead.Seal(nil, nonce, plaintext, additionalData)
+
+	// Decrypt and authentiate |ciphertext| and authenticate
+	// |additionalData|.
+	plaintext, err = aead.Open(nil, nonce, ciphertext, additionalData)
+	if err != nil {
+		// Authentication failed. Either the ciphertext or
+		// additionalData (or both) were invalid for the 
+		// (key, nonce) pair.
+		[...]
+	}
+}
+```
+
+## Security
+
+### Disclosure
+
+This project uses full disclosure. If you find a security bug in
+an implementation, please e-mail me or create a GitHub issue.
+
+### Disclaimer
+
+You should only use cryptography libraries that have been
+reviewed by cryptographers or cryptography engineers. While I am
+a cryptography engineer, I'm not your cryptography engineer, and
+I have not had this project reviewed by any other cryptographers.
--- ascon/fuzz_test.go
+++ ascon/fuzz_test.go
@@ -1,72 +1,127 @@
 //go:build fuzz
 
-package ascon
+package ascon_test
 
 import (
 	"bytes"
-	"math/rand"
+	"crypto/cipher"
+	"crypto/rand"
+	"math/bits"
 	"os"
 	"testing"
 	"time"
 
-	ascon2 "lukechampine.com/ascon"
+	"github.com/ericlagergren/lwcrypto/ascon"
+	ref "github.com/ericlagergren/lwcrypto/ascon/internal/asconc/ref"
+	refa "github.com/ericlagergren/lwcrypto/ascon/internal/asconc/refa"
 )
 
 func TestFuzz(t *testing.T) {
+	t.Run("128", func(t *testing.T) {
+		t.Parallel()
+
+		testFuzz(t, ref.New, ascon.New128)
+	})
+	t.Run("128a", func(t *testing.T) {
+		t.Parallel()
+
+		testFuzz(t, refa.New, ascon.New128a)
+	})
+}
+
+func testFuzz(t *testing.T, ref, test func([]byte) (cipher.AEAD, error)) {
 	d := 2 * time.Second
+	if testing.Short() {
+		d = 10 * time.Millisecond
+	}
 	if s := os.Getenv("ASCON_FUZZ_TIMEOUT"); s != "" {
 		var err error
 		d, err = time.ParseDuration(s)
 		if err != nil {
 			t.Fatal(err)
 		}
-	} else if testing.Short() {
-		d = 10 * time.Millisecond
 	}
 	tm := time.NewTimer(d)
 
-	key := make([]byte, KeySize)
-	nonce := make([]byte, NonceSize)
-	plaintext := make([]byte, (BlockSize128*3)+BlockSize128-3)
-	for {
+	key := make([]byte, ascon.KeySize)
+	nonce := make([]byte, ascon.NonceSize)
+	plaintext := make([]byte, 1*1024*1024) // 1 MB
+	for i := 0; ; i++ {
 		select {
 		case <-tm.C:
+			t.Logf("iters: %d", i)
 			return
 		default:
 		}
+
 		if _, err := rand.Read(key); err != nil {
-			t.Fatal(err)
-		}
-		wantAead, err := ascon2.New(key)
-		if err != nil {
-			t.Fatal(err)
-		}
-		gotAead, err := New128(key)
-		if err != nil {
 			t.Fatal(err)
 		}
 		if _, err := rand.Read(nonce); err != nil {
 			t.Fatal(err)
 		}
-		if _, err := rand.Read(plaintext); err != nil {
+		n := randIntn(len(plaintext))
+		if _, err := rand.Read(plaintext[:n]); err != nil {
 			t.Fatal(err)
 		}
-		want := wantAead.Seal(nil, nonce, plaintext, nil)
-		got := gotAead.Seal(nil, nonce, plaintext, nil)
-		if !bytes.Equal(want, got) {
-			t.Fatalf("expected %#x, got %#x", want, got)
-		}
-		ciphertext := want
-		want, err = wantAead.Open(nil, nonce, ciphertext, nil)
+		plaintext := plaintext[:n]
+
+		refAead, err := ref(key)
 		if err != nil {
 			t.Fatal(err)
 		}
-		got, err = gotAead.Open(nil, nonce, ciphertext, nil)
+		gotAead, err := test(key)
 		if err != nil {
 			t.Fatal(err)
 		}
-		if !bytes.Equal(want, got) {
-			t.Fatalf("expected %#x, got %#x", want, got)
+
+		wantCt := refAead.Seal(nil, nonce, plaintext, nil)
+		gotCt := gotAead.Seal(nil, nonce, plaintext, nil)
+		if !bytes.Equal(wantCt, gotCt) {
+			t.Fatalf("expected %#x, got %#x", wantCt, gotCt)
+		}
+
+		wantPt, err := refAead.Open(nil, nonce, wantCt, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotPt, err := gotAead.Open(nil, nonce, wantCt, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(wantPt, gotPt) {
+			t.Fatalf("expected %#x, got %#x", wantPt, gotPt)
+		}
+	}
+}
+
+// randIntn returns a random integer in [0, max).
+func randIntn(max int) int {
+	if max <= 0 {
+		panic("max <= 0")
+	}
+	bitLen := bits.Len64(uint64(max - 1))
+	if bitLen == 0 {
+		return 0
+	}
+	k := (bitLen + 7) / 8
+	b := uint(bitLen % 8)
+	if b == 0 {
+		b = 8
+	}
+	buf := make([]byte, k)
+	for i := 0; ; i++ {
+		if _, err := rand.Read(buf); err != nil {
+			panic(err)
+		}
+		buf[0] &= byte(int(1<<b) - 1)
+		var x, s uint64
+		for i := len(buf) - 1; i >= 0; i-- {
+			x |= uint64(buf[i]) << s
+			s += 8
+		}
+		if x < uint64(max) {
+			return int(x)
 		}
 	}
 }
