955bb7520057526fdb03e50eef1452ee74f01130
Eric Lagergren <eric@ericlagergren.com>
2021-12-27 20:46:23 -0800

    use github.com/ericlagergren/saferand
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- ascon/fuzz_test.go
+++ ascon/fuzz_test.go
@@ -5,8 +5,6 @@ package ascon_test
 import (
 	"bytes"
 	"crypto/cipher"
-	"crypto/rand"
-	"math/bits"
 	"os"
 	"testing"
 	"time"
@@ -14,6 +12,7 @@ import (
 	"github.com/ericlagergren/lwcrypto/ascon"
 	ref "github.com/ericlagergren/lwcrypto/ascon/internal/asconc/ref"
 	refa "github.com/ericlagergren/lwcrypto/ascon/internal/asconc/refa"
+	rand "github.com/ericlagergren/saferand"
 )
 
 func TestFuzz(t *testing.T) {
@@ -60,7 +59,7 @@ func testFuzz(t *testing.T, ref, test func([]byte) (cipher.AEAD, error)) {
 		if _, err := rand.Read(nonce); err != nil {
 			t.Fatal(err)
 		}
-		n := randIntn(len(plaintext))
+		n := rand.Intn(len(plaintext))
 		if _, err := rand.Read(plaintext[:n]); err != nil {
 			t.Fatal(err)
 		}
@@ -91,37 +90,6 @@ func testFuzz(t *testing.T, ref, test func([]byte) (cipher.AEAD, error)) {
 		}
 		if !bytes.Equal(wantPt, gotPt) {
 			t.Fatalf("expected %#x, got %#x", wantPt, gotPt)
-		}
-	}
-}
-
-// randIntn returns a random integer in [0, max).
-func randIntn(max int) int {
-	if max <= 0 {
-		panic("max <= 0")
-	}
-	bitLen := bits.Len64(uint64(max - 1))
-	if bitLen == 0 {
-		return 0
-	}
-	k := (bitLen + 7) / 8
-	b := uint(bitLen % 8)
-	if b == 0 {
-		b = 8
-	}
-	buf := make([]byte, k)
-	for i := 0; ; i++ {
-		if _, err := rand.Read(buf); err != nil {
-			panic(err)
-		}
-		buf[0] &= byte(int(1<<b) - 1)
-		var x, s uint64
-		for i := len(buf) - 1; i >= 0; i-- {
-			x |= uint64(buf[i]) << s
-			s += 8
-		}
-		if x < uint64(max) {
-			return int(x)
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ericlagergren/lwcrypto
 
 go 1.16
+
+require github.com/ericlagergren/saferand v0.0.0-20211228043234-529f04ad6e1a
