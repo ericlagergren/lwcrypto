bb3c85e3815a47e10bec8e2ae4e46165ffa0283f
Eric Lagergren <eric@ericlagergren.com>
2022-02-13 13:40:52 -0800

    use EON instead of MVN + EOR
    Increases perf by ~10 MB/s.
    
    Signed-off-by: Eric Lagergren <eric@ericlagergren.com>

--- ascon/ascon_arm64.s
+++ ascon/ascon_arm64.s
@@ -36,46 +36,41 @@
 //
 // Uses s{0,1,2,3,4} and t{0,1,2,3,4}.
 #define ROUND(C) \
-	/* Round constant */                             \
-	EOR C, s2, s2; /* s2 ^= C */                     \
-	                                                 \
-	/* Substitution */                               \
-	EOR s4, s0, s0; /* s0 ^= s4 */                   \
-	EOR s3, s4, s4; /* s4 ^= s3 */                   \
-	EOR s1, s2, s2; /* s2 ^= s1 */                   \
-	                                                 \
-	/* Keccak S-box */                               \
-	BIC s1, s2, t0; /* t0 := ^s1 & s2 */             \
-	BIC s2, s3, t1; /* t1 := ^s2 & s3 */             \
-	BIC s3, s4, t2; /* t2 := ^s3 & s4 */             \
-	BIC s4, s0, t3; /* t3 := ^s4 & s0 */             \
-	BIC s0, s1, t4; /* t4 := ^s0 & s1 */             \
-	                                                 \
-	EOR s0, t0, t0; /* t0 ^= s0 */                   \
-	EOR s1, t1, t1; /* t1 ^= s1*/                    \
-	EOR s2, t2, t2; /* t2 ^= s2 */                   \
-	EOR s3, t3, t3; /* t3 ^= s3 */                   \
-	EOR s4, t4, t4; /* t4 ^= s4 */                   \
-	                                                 \
-	/* Substitution */                               \
-	EOR t0, t1, t1; /* t1 ^= t0 */                   \
-	EOR t4, t0, t0; /* t0 ^= t4 */                   \
-	EOR t2, t3, t3; /* t3 ^= t2 */                   \
-	MVN t2, t2; /* t2 = ^t2 */                       \
-	                                                 \
-	EOR t0@>19, t0, s0; /* s0 = t0 ^ rotr(t0, 19) */ \
-	EOR t1@>61, t1, s1; /* s1 = t1 ^ rotr(t1, 61) */ \
-	EOR t2@>1, t2, s2; /* s2 = t2 ^ rotr(t2, 1) */   \
-	EOR t3@>10, t3, s3; /* s3 = t3 ^ rotr(t3, 10) */ \
-	EOR t4@>7, t4, s4; /* s4 = t4 ^ rotr(t4, 7) */   \
-	                                                 \
-	EOR t0@>28, s0, s0; /* s0 ^= rotr(t0, 28) */     \
-	EOR t1@>39, s1, s1; /* s1 ^= rotr(t1, 39) */     \
-	EOR t2@>6, s2, s2; /* s2 ^= rotr(t2, 6) */       \
-	EOR t3@>17, s3, s3; /* s3 ^= rotr(t3, 17) */     \
-	EOR t4@>41, s4, s4; /* s4 ^= rotr(t4, 41) */     \
-	                                                 \
-	/* So the comments line up correctly */          \
+	EOR C, s2, s2 /* s2 ^= C */                           \
+	                                                      \
+	EOR s4, s0, s0 /* s0 ^= s4 */                         \
+	EOR s3, s4, s4 /* s4 ^= s3 */                         \
+	EOR s1, s2, s2 /* s2 ^= s1 */                         \
+	                                                      \
+	BIC s1, s2, t0 /* t0 := ^s1 & s2 */                   \
+	BIC s3, s4, t2 /* t2 := ^s3 & s4 */                   \
+	BIC s0, s1, t4 /* t4 := ^s0 & s1 */                   \
+	BIC s2, s3, t1 /* t1 := ^s2 & s3 */                   \
+	BIC s4, s0, t3 /* t3 := ^s4 & s0 */                   \
+	                                                      \
+	EOR s0, t0, t0 /* t0 ^= s0 */                         \
+	EOR s1, t1, t1 /* t1 ^= s1*/                          \
+	EOR s2, t2, t2 /* t2 ^= s2 */                         \
+	EOR s3, t3, t3 /* t3 ^= s3 */                         \
+	EOR s4, t4, t4 /* t4 ^= s4 */                         \
+	                                                      \
+	EOR t0, t1, t1 /* t1 ^= t0 */                         \
+	EOR t2, t3, t3 /* t3 ^= t2 */                         \
+	EOR t4, t0, t0 /* t0 ^= t4 */                         \
+	                                                      \
+	EOR t2@>6-1, t2, s2 /* s2 = t2 ^ rotr(t2, 6-1) */     \
+	EOR t3@>17-10, t3, s3 /* s3 = t3 ^ rotr(t3, 17-10) */ \
+	EOR t4@>41-7, t4, s4 /* s4 = t4 ^ rotr(t4, 41-8) */   \
+	EOR t0@>28-19, t0, s0 /* s0 = t0 ^ rotr(t0, 28-19) */ \
+	EOR t1@>61-39, t1, s1 /* s1 = t1 ^ rotr(t1, 61-39) */ \
+	                                                      \
+	EOR s3@>10, t3, s3 /* s3 = t3 ^ rotr(s3, 10) */       \
+	EOR s4@>7, t4, s4 /* s4 = t4 ^ rotr(s4, 7) */         \
+	EOR s0@>19, t0, s0 /* s0 = t0 ^ rotr(s0, 19) */       \
+	EOR s1@>39, t1, s1 /* s1 = t1 ^ rotr(s1, 39) */       \
+	EON s2@>1, t2, s2 /* s2 = t2 ^ rotr(s2, 1) */         \
+	                                                      \
+	/* So the comments line up correctly */               \
 	NOP
 
 #define P12 \
